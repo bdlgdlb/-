9c8f192dbf30cee5eff0ff0556d767ac4767d53c
wangdun <1056102798@qq.com>
2017-10-04 20:24:14 +0800

    12.1

---
+++ 12.01/Exercise12_01_1.java
@@ -0,0 +1,35 @@
+public class Exercise12_01_1{
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.out.println(
+				"Usage: java Calculator operand1 operator operand2");
+			System.exit(0);
+		}
+
+		int result = 0;
+		try {
+			switch (args[1].charAt(0)) {
+				case '+' : result = Integer.parseInt(args[0]) +
+										  Integer.parseInt(args[2]);
+							  break;
+				case '-' : result = Integer.parseInt(args[0]) -
+										  Integer.parseInt(args[2]);
+							  break;
+				case '.' : result = Integer.parseInt(args[0]) *
+										  Integer.parseInt(args[2]);
+							  break;
+				case '/' : result = Integer.parseInt(args[0]) /
+										  Integer.parseInt(args[2]);
+							  break;
+			}	
+
+			System.out.println(args[0] + ' ' + args[1] + ' ' + args[2]
+				+ " = " + result);
+		}
+		catch (NumberFormatException ex) {
+			System.out.println("Wrong Input: " + 
+				ex.getMessage().substring(ex.getMessage().indexOf("\"") + 1, 
+					ex.getMessage().lastIndexOf("\"")));
+		}
+	}
+}
