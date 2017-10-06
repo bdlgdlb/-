42ba9c69a68ae712ceb3bbf9cb467ab32155defc
wangdun <1056102798@qq.com>
2017-10-06 20:23:39 +0800

    12.8

---
+++ 12.08/HexFormatException.java
@@ -0,0 +1,12 @@
+public class HexFormatException extends NumberFormatException {
+	private String hex;
+
+	public HexFormatException(String hex) {
+		super(hex + " is not a hex string");
+		this.hex = hex;
+	} 
+
+	public String getHex() {
+		return hex;
+	}
+}
