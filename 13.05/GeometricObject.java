597f8baa1c1f1f3c1671a9629a35c4bd21c668ac
wangdun <1056102798@qq.com>
2017-10-13 16:41:22 +0800

    13.05

---
+++ 13.05/GeometricObject.java
@@ -0,0 +1,59 @@
+public abstract class GeometricObject implements Comparable<GeometricObject>{
+	private String color = "while";
+	private boolean filled;
+	private java.util.Date dateCreated;
+
+	protected GeometricObject() {
+		dateCreated = new java.util.Date();
+	}
+
+	protected GeometricObject(String color, boolean filled) {
+		dateCreated = new java.util.Date();
+		this.color = color;
+		this.filled = filled;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isFilled() {
+		return filled;
+	}
+
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+
+	public java.util.Date getDateCreated() {
+		return dateCreated;
+	}
+
+	@Override
+	public String toString() {
+		return "created on " + dateCreated + "\ncolor: " + color +
+			" and filled: " + filled;
+	}
+
+	@Override
+	public int compareTo(GeometricObject o){
+		if (this.getArea() > o.getArea()) 
+			return 1;
+		else if (this.getArea() == o.getArea())
+			return 0;
+		else
+			return -1;
+	}
+
+	public static GeometricObject max(GeometricObject o1, GeometricObject o2) {
+		return o1.compareTo(o2) == 1 ? o1 : o2;
+	}
+
+	public abstract double getArea();
+
+	public abstract double getPerimeter();
+}
