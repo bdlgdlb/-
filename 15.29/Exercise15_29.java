55a16bc1505c379856368be31231ce22b4e9dcbd
wangdun <1056102798@qq.com>
2017-12-11 14:09:47 +0800

    15.29

---
+++ 15.29/Exercise15_29.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
+
+public class Exercise15_29 extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		CarPane pane = new CarPane();
+
+		pane.setOnMousePressed(e -> pane.pause());
+		pane.setOnMouseReleased(e -> pane.play());
+
+		pane.setOnKeyPressed(e -> {
+			if (e.getCode() == KeyCode.UP) {
+				pane.increaseSpeed();
+			}
+			else if (e.getCode() == KeyCode.DOWN) {
+				pane.decreaseSpeed();
+			}
+		});
+
+		Scene scene = new Scene(pane, 600, 100);
+		primaryStage.setTitle("Exercise15_29"); 
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		pane.requestFocus();
+	}
+}
